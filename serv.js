24f300aca26cf0fd876dec2c69541a84bf992eb3
JohanHullot <johan.hullot@gmail.com>
2024-11-30 13:36:48 +0100

    Initial Commit

---
+++ serv.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const hbs = require("hbs")
+const path = require("path");
+
+app = express();
+prisma = new PrismaClient();
+PORT = 3015;
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+const barbarreeee = await prisma.barbarreeee.findMany();
+
+app.get("/", async (req, res) =>
+{
+
+    res.render("index",(barbarreeee));
+    console.log(barbarreeee);
+    // const barbare = {
+    // name: "Inception",
+    // born: new Date(),
+    // };
+
+    // const newbarbare = await prisma.barbarreeee.create({
+    //     data: barbare,
+    // }); // retourne l'objet Movie créé avec son ID
+
+    // res.json(barbarreeee);
+})
+
+app.get("/drop", async (req, res) => 
+{
+    //await prisma.barbarreeee.delete()
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
