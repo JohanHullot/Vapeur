7c9fb17c462c3d87b9b33ada93705de3ef0ed0ce
JohanHullot <johan.hullot@gmail.com>
2024-12-04 17:28:55 +0100

    Finition de addGame/editor
    Ajout de test des erreurs possibles

--- serv.js
+++ serv.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const hbs = require("hbs");
 const path = require("path");
-//const utils = require("./utils.js"); //import local des fonctions js
+const utils = require("./utils.js"); //import local des fonctions js
 //
 //Preparation des modules
 app = express();
@@ -14,7 +14,9 @@ hbs.registerPartials(path.join(__dirname, "/views/partials")); //Donne les chemi
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views")); //Donne le chemin des views que va utiliser app
 
-app.use(express.static("public"));//Fichier de /public/ deviennent visible pour toutes les routes
+app.use(express.static("public")); //Fichier de /public/ deviennent visible pour toutes les routes
+
+app.use(express.urlencoded({ extended: true })); // permet de recevoir les posts des forms
 
 //Routes
 
@@ -59,8 +61,6 @@ app.get("/Category/:name", async (req, res) => {  //Prends les pages par categor
 
 
 
-
-
 //GAME
 app.get("/Game", async (req, res) => {
     const game = await prisma.Game.findMany();
@@ -96,20 +96,42 @@ app.get("/addGame", async (req, res) => {
     });
 });
 
-app.post("/addGame", async (req, res) => {
+app.post("/addGame", async (req, res) => {  //recois le form
 
-    //AJOUT DANS LA DB    
+    let isError = false;
+    try
+    {
+        const gameSameName = await prisma.Game.findMany({where: {name: req.body.name}});
 
+        if(gameSameName[0]) //Erreur Le jeu à le meme nom
+        {
+            isError = true;
+            const nameError = "Le nom du jeu existe déjà";
+            res.render("Game/errorAdd", {
+                nameError
+            });
+        }
+        else //sinon implementation db
+        {
+            utils.addGame(req.body);
+            console.log("Pas de problème rencontrer");
+        }
+    }
+    catch(err) //Si le try a planté
+    {
+        isError = true;
+        console.error("Erreur dans l'ajout du jeu", err)
+        const nameError = "Une erreur serveur a été detecté";
+        res.render("Game/errorAdd", {
+            nameError
+        });
+    }
 
-
-    const category = await prisma.Category.findMany();
-    const editor = await prisma.Editor.findMany();
-    res.render("Game/addGame",{    //index.hbs est afficher sur le site
-        category,
-        editor
-    });
+    if (!isError) //Si il y a eu une erreur
+    {
+        res.redirect("/Game");
+    }
 });
-
 
 
 //EDITOR
@@ -134,6 +156,49 @@ app.get("/Editor/:name", async (req, res) => {  //Prends les pages par editeur g
     {res.send()}
 });
 
+app.get("/addEditor", (req, res) => {
+    res.render("Editor/addEditor",{});
+});
+
+app.post("/addEditor", async (req, res) => {  //recois le form
+
+    let isError = false;
+    try
+    {
+        const editorSameName = await prisma.Editor.findMany({where: {name: req.body.name}});
+
+        if(editorSameName[0]) //Erreur Le jeu à le meme nom
+        {
+            isError = true;
+            const nameError = "L'éditeur existe déjà";
+            res.render("Editor/errorAdd", {
+                nameError
+            });
+        }
+        else //sinon implementation db
+        {
+            utils.addEditor(req.body);
+            console.log("Pas de problème rencontrer");
+        }
+    }
+    catch(err) //Si le try a planté
+    {
+        isError = true;
+        console.error("Erreur dans l'ajout de l'éditeur", err)
+        const nameError = "Une erreur serveur a été detecté";
+        res.render("Game/errorAdd", {
+            nameError
+        });
+    }
+
+    if (!isError) //Si il y a eu une erreur
+    {
+        res.redirect("/Editor");
+    }
+});
+
+//Handicapy
+//Vous aimez les capybaras ? Oui, alors ce jeu est fait pour vous ! Dans Handicapy vous incarnez un capybara, votre but est d'aider des capybara handicapé et de combattre des prédateurs pour avancer dans le jeu !
 
 //Connexion au Port 3015
 app.listen(PORT, () => {
