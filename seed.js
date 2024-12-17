6e1e346784dbf16514891269d4c0856c9e2a430c
JohanHullot <johan.hullot@gmail.com>
2024-12-17 16:32:08 +0100

    clear data

--- seed.js
+++ seed.js
@@ -46,9 +46,9 @@ async function createEditorDefault() //creer les editeurs demandé dans la db
 async function createGameDefault() //creer les jeux demandé dans la db
 {
     const tabGame = [["Faster Than Light",4,3,"Ce jeu de simulation spatiale de type rogue-like vous permet de piloter votre vaisseau dans une galaxie générée aléatoirement où vous pourrez vous couvrir de gloire... si vous parvenez à éviter la défaite.",""],
-                        ["Into the Breach",1,3,"Contrôlez de puissants Mechas venus du futur pour vaincre une terrible menace extraterrestre. Chaque tentative faite pour sauver le monde est un nouveau défi généré aléatoirement dans ce jeu de stratégie au tour par tour.",""],
+                        ["Into the Breach",1,3,"Contrôlez de puissants Mechas venus du futur pour vaincre une terrible menace extraterrestre. Chaque tentative faite pour sauver le monde est un nouveau défi généré aléatoirement dans ce jeu de stratégie au tour par tour.","paulnareff.png"],
                         ["Muck",3,2,"Muck est un jeu de type roguelike de survie. Collectez des ressources, trouvez des objets et construisez une base pour survivre aussi longtemps que possible.","joseph joestar.PNG"],
-                        ["Brotato",1,1,"Brotato est un roguelite où vous incarnez une pomme de terre maniant jusqu'à 6 armes à la fois pour combattre des hordes d'extraterrestres. Choisissez parmi une variété de traits et d'objets pour créer des parties uniques et survivre jusqu'à l'arrivée des secours.",""],
+                        ["Brotato",1,1,"Brotato est un roguelite où vous incarnez une pomme de terre maniant jusqu'à 6 armes à la fois pour combattre des hordes d'extraterrestres. Choisissez parmi une variété de traits et d'objets pour créer des parties uniques et survivre jusqu'à l'arrivée des secours.","brotato.PNG"],
                         ["Minecraft",2,4,"Minecraft est un jeu d'aventure type bac à sable dans lequel vous pouvez créer des mondes et affronter des monstres.",""]];
 
     for (indice in tabGame) //pour chaque catégorie
