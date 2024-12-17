dd9bc35fa3546e6e1b899da6d4b2c13d83a5b42d
JohanHullot <johan.hullot@gmail.com>
2024-12-17 16:00:59 +0100

    CLEAR SEED

--- seed.js
+++ seed.js
@@ -1,3 +1,6 @@
+const { PrismaClient } = require("@prisma/client");
+prisma = new PrismaClient();
+
 async function createCategoryDefault() //creer les catégories demandé dans la db
 {
     const tabCategory = ["Action",
@@ -17,29 +20,7 @@ async function createCategoryDefault() //creer les catégories demandé dans la 
         });
         console.log(tabCategory[indice]);
     }
-}
-
-async function createGameDefault() //creer les jeux demandé dans la db
-{
-    const tabGame = [["Faster Than Light",4,3,"Ce jeu de simulation spatiale de type rogue-like vous permet de piloter votre vaisseau dans une galaxie générée aléatoirement où vous pourrez vous couvrir de gloire... si vous parvenez à éviter la défaite."],
-                        ["Into the Breach",1,3,"Contrôlez de puissants Mechas venus du futur pour vaincre une terrible menace extraterrestre. Chaque tentative faite pour sauver le monde est un nouveau défi généré aléatoirement dans ce jeu de stratégie au tour par tour."],
-                        ["Muck",3,2,"Muck est un jeu de type roguelike de survie. Collectez des ressources, trouvez des objets et construisez une base pour survivre aussi longtemps que possible."],
-                        ["Brotato",1,1,"Brotato est un roguelite où vous incarnez une pomme de terre maniant jusqu'à 6 armes à la fois pour combattre des hordes d'extraterrestres. Choisissez parmi une variété de traits et d'objets pour créer des parties uniques et survivre jusqu'à l'arrivée des secours."],
-                        ["Minecraft",2,4,"Minecraft est un jeu d'aventure type bac à sable dans lequel vous pouvez créer des mondes et affronter des monstres."]];
-
-    for (indice in tabGame) //pour chaque catégorie
-    {
-        const game = {
-        name: tabGame[indice][0],
-        gameCategory: tabGame[indice][1],
-        gameEditor: tabGame[indice][2],
-        description: tabGame[indice][3]
-        };
-        const newgame = await prisma.Game.create({ //Creation des jeux dans la db
-            data: game,
-        });
-        console.log(tabGame[indice]);
-    }
+    createEditorDefault();
 }
 
 async function createEditorDefault() //creer les editeurs demandé dans la db
@@ -59,8 +40,33 @@ async function createEditorDefault() //creer les editeurs demandé dans la db
         });
         console.log(tabEditor[indice]);
     }
+    createGameDefault();
 }
 
+async function createGameDefault() //creer les jeux demandé dans la db
+{
+    const tabGame = [["Faster Than Light",4,3,"Ce jeu de simulation spatiale de type rogue-like vous permet de piloter votre vaisseau dans une galaxie générée aléatoirement où vous pourrez vous couvrir de gloire... si vous parvenez à éviter la défaite.",""],
+                        ["Into the Breach",1,3,"Contrôlez de puissants Mechas venus du futur pour vaincre une terrible menace extraterrestre. Chaque tentative faite pour sauver le monde est un nouveau défi généré aléatoirement dans ce jeu de stratégie au tour par tour.",""],
+                        ["Muck",3,2,"Muck est un jeu de type roguelike de survie. Collectez des ressources, trouvez des objets et construisez une base pour survivre aussi longtemps que possible.","joseph joestar.PNG"],
+                        ["Brotato",1,1,"Brotato est un roguelite où vous incarnez une pomme de terre maniant jusqu'à 6 armes à la fois pour combattre des hordes d'extraterrestres. Choisissez parmi une variété de traits et d'objets pour créer des parties uniques et survivre jusqu'à l'arrivée des secours.",""],
+                        ["Minecraft",2,4,"Minecraft est un jeu d'aventure type bac à sable dans lequel vous pouvez créer des mondes et affronter des monstres.",""]];
+
+    for (indice in tabGame) //pour chaque catégorie
+    {
+        const game = {
+        name: tabGame[indice][0],
+        gameCategory: tabGame[indice][1],
+        gameEditor: tabGame[indice][2],
+        description: tabGame[indice][3],
+        nameOfImage: tabGame[indice][4]
+        };
+        const newgame = await prisma.Game.create({ //Creation des jeux dans la db
+            data: game,
+        });
+        console.log(tabGame[indice]);
+    }
+}
+
+
 createCategoryDefault();
-createEditorDefault();
-createGameDefault();
+
